905d98a1974d29a7838f7b7f296051b0dba1227f
Marbo Cheng Ye <marbochengye@gmail.com>
2019-05-31 21:07:17 -0700

    Proxy with ECs2

--- server.js
+++ server.js
@@ -9,28 +9,4 @@ app.use(cors());
 
 app.use(express.static('public'));
 
-// app.get('/listing', (req, res) => {
-//     console.log('get listing');
-//     request('http://localhost:3005/listing').pipe(res);
-//     console.log('got listing');
-// });
-  
-// app.get('/bookings', (req, res) => {
-//     console.log('get calendar');
-//     request('http://localhost:3000/bookings').pipe(res);
-//     console.log('got calendar');
-// });
-  
-// app.get('/reviews', (req, res) => {
-//   console.log('get reviews');
-//   request('http://localhost:3001/reviews').pipe(res);
-//   console.log('got reviews');
-// });
-  
-// app.get('/photoGallery', (req, res) => {
-//   console.log('get photos');
-//   request('http://localhost:3003/photoGallery').pipe(res);
-//   console.log('got photos');
-// });
-
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
